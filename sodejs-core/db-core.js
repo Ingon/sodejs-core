414a8ba599f015771aa30493bb7e8fabfd5c8737
sungon@gmail.com <sungon@gmail.com@e4074e52-2194-11df-9e09-9f768d97ad4a>
2010-02-25 12:02:33 +0000

    core migrations in place

--- sodejs-core/db-core.js
+++ sodejs-core/db-core.js
@@ -1,12 +1,55 @@
+function Table(tables) {
+	this.name = tables.getString('TABLE_NAME');
+}
+
+function Connection(jcon) {
+	this.jcon = jcon;
+	
+	this.tables = {};
+	var dbmeta = jcon.getMetaData();
+	var tables = dbmeta.getTables(null, 'public', null, ['TABLE']);
+	while(tables.next()) {
+		var t = new Table(tables)
+		this.tables[t.name] = t;
+	}
+
+	function createStatement(sql, arguments) {
+		var arr = arguments;
+		if(arguments.length == 2 && (arguments[1] instanceof Array)) {
+			arr = arguments[1];
+			var stat = jcon.prepareStatement(sql);
+			for(var i = 0; i < arr.length;i++) {
+				stat.setObject(i + 1, arr[i]);
+			}
+			return stat;
+		} else {
+			var stat = jcon.prepareStatement(sql);
+			for(var i = 1; i < arr.length;i++) {
+				stat.setObject(i, arr[i]);
+			}
+			return stat;
+		}
+	};
+	
+	this.query = function(sql) {
+		return createStatement(sql, arguments).executeQuery();
+	};
+	
+	this.update = function(sql) {
+		return createStatement(sql, arguments).executeUpdate();
+	};
+}
+
 exports.pgLoad = function() {
-	Packages.java.lang.Class.forName("org.postgresql.Driver");
+	Packages.java.lang.Class.forName('org.postgresql.Driver');
 }
 
 exports.pgConnect = function(db, user, pass, host, port) {
 	user = user || db;
 	pass = pass || db;
-	host = host || "localhost";
-	port = port || "5432";
-	var url = "jdbc:postgresql://" + host + ":" + port + "/" + db;
-	Packages.java.sql.DriverManager.getConnection(url, user, pass);
+	host = host || 'localhost';
+	port = port || '5432';
+	var url = 'jdbc:postgresql://' + host + ':' + port + '/' + db;
+	var jcon = Packages.java.sql.DriverManager.getConnection(url, user, pass);
+	return new Connection(jcon);
 }
--- sodejs-core/db-migrations.js
+++ sodejs-core/db-migrations.js
@@ -1,5 +1,30 @@
-var log = require("sys-log");
+var versionTableName = 'sys_version';
+var versions = [];
 
-exports.test = function() {
-	log.trace("db-migrations test");
-}
+exports.version = function(ver, name, callback) {
+	versions[ver] = {
+		name : name,
+		callback : callback
+	};
+};
+
+exports.migrate = function(con) {
+	var currentVersion = -1;
+	if(con.tables[versionTableName]) {
+		var rs = con.query('SELECT max(number) FROM ' + versionTableName);
+		if(rs.next()) {
+			currentVersion = rs.getInt(1);
+		}
+	} else {
+		con.update('CREATE TABLE ' + versionTableName + ' (number INTEGER, name VARCHAR(255), PRIMARY KEY (number))')
+	}
+	
+	require('sys-log').trace('Version: ' + currentVersion);
+	
+	for(var i = currentVersion + 1; i < versions.length; i++) {
+		if(versions[i]) {
+			versions[i].callback(con);
+			con.update("INSERT INTO " + versionTableName + " (number, name) VALUES (?, ?)", i, versions[i].name);
+		}
+	}
+};
