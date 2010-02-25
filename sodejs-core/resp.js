75fe610f7c2e79841aa2b0ee64fbc7473b21d613
sungon@gmail.com <sungon@gmail.com@e4074e52-2194-11df-9e09-9f768d97ad4a>
2010-02-25 10:48:22 +0000

    module system in place

--- sodejs-core/resp.js
+++ sodejs-core/resp.js
@@ -1,10 +1,8 @@
-var resp = {
-	write : function(text) {
-		_res.getWriter().write(text);
-	},
+exports.write = function(text) {
+	_res.getWriter().write(text);
+};
 
-	flush : function() {
-		_res.getWriter().flush();
-		_res.getWriter().close();
-	},
-}
+exports.flush = function() {
+	_res.getWriter().flush();
+	_res.getWriter().close();
+};
